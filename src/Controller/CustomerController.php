d93bb42f776c5f0c75f47fe69490374090a26bb5
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-08-01 16:52:13 +0300

    Send form

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
 use App\Form\CustomerType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CustomerController extends AbstractController
 {
     #[Route('/customer', name: 'app_customer')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $form = $this->createForm(CustomerType::class);
+
+        $customer = new Customer();
+        $form = $this->createForm(CustomerType::class,$customer);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($request->request->all());
+        }
         return $this->render('customer/index.html.twig', [
             'form' => $form->createView(),
         ]);
